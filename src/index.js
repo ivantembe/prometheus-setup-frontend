5c66d432d2afe51deb5391ec566aa4ab2937c38a
Ivan Tembe <ivantembe@digitalneurs1.speedport.ip>
2022-12-30 18:52:31 +0100

    push to gitHub

--- src/index.js
+++ src/index.js
@@ -3,11 +3,34 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MetricsProvider } from '@cabify/prom-react';
+import prom from 'promjs';
+
+// const registry = prom();
+// const pageRequestCounter = registry.create(
+//   'counter',
+//   'page_requests',
+//   'A counter for page requests'
+// );
+
+// pageRequestCounter.inc();
+// console.log(registry.metrics());
+// fetch('http://localhost:9090/metrics', {
+//   method: 'post',
+//   body: registry.metrics(),
+// });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MetricsProvider
+      appName="my-app-name"
+      owner="my-team"
+      // getNormalizedPath={normalizePath}
+      metricsAggregatorUrl="http://localhost:9090/metrics"
+    >
+      <App />
+    </MetricsProvider>
   </React.StrictMode>
 );
 
